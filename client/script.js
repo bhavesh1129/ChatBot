2843c5bc62cc671e8c5481c2d298bce14f20e9a4
bhavesh1129 <bhavesh0336.cse19@chitkara.edu.in>
2022-12-28 14:25:54 +0530

    Update changes

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const messageDiv = document.getElementById(uniqueId);
 loader(messageDiv);
 
 
-const response = await fetch("http://localhost:5000/", {
+const response = await fetch("https://chatbot-sijj.onrender.com/", {
   method:"POST",
   headers:{
     "Content-Type":"application/json",
